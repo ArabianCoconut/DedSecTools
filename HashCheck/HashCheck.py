fbb7f5c3c7f07fba1a0a4bccf114bed011fbc091
DedSec <16560968+advisor25@users.noreply.github.com>
2018-04-06 06:37:24 +0400

    Add files via upload

---
+++ HashCheck/HashCheck.py
@@ -0,0 +1,36 @@
+import hashlib # NOTE:  Imported Library like hashlib and Sys all native Python libs made in Python3!
+import sys
+
+
+print('Welcome to HashCompare!\n'
+'Use the File path to compare your hashes!\n'
+'If you dont want to Compare just leave it blank or type y ! \n\n')
+
+
+
+# NOTE: this is md5 hash code section!
+
+Filename = input("Enter the name of the file:")
+compare=input('CompareFile:')
+openfile = open(Filename)
+readme = openfile.read(128)
+trans=readme.encode('utf8')
+md5 = hashlib.md5()
+md5.update(trans)
+print("MD5: {0}".format(md5.hexdigest()))
+print('\n\n')
+
+if compare == 'y': # NOTE:  this Section contains the code to compare 2 Files in MD5! if you want Sha1 just change all .md5() to .sha1()
+    input=input('Filename to compare:')
+    open=open(input)
+    reading=open.read(128)
+    encoding=reading.encode('utf8')
+    hash=hashlib.md5()
+    hash.update(encoding)
+    print('MD5:{0}'.format(md5.hexdigest()))
+    print('\n')
+    print('MD5:{0}'.format(hash.hexdigest()))
+    print('\n')
+    print('Done!')
+
+# NOTE: End of Coding!
