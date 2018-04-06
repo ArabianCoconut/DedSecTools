d3320d82dcac027999369492f9778f782d25480f
DedSec <16560968+advisor25@users.noreply.github.com>
2018-04-06 06:51:07 +0400

    Update HashCheck.py

--- HashCheck/HashCheck.py
+++ HashCheck/HashCheck.py
@@ -10,7 +10,7 @@ print('Welcome to HashCompare!\n'
 
 # NOTE: this is md5 hash code section!
 
-Filename = input("Enter the name of the file:")
+Filename = input("Enter PATH to File:")
 compare=input('CompareFile:')
 openfile = open(Filename)
 readme = openfile.read(128)
@@ -21,7 +21,7 @@ print("MD5: {0}".format(md5.hexdigest()))
 print('\n\n')
 
 if compare == 'y': # NOTE:  this Section contains the code to compare 2 Files in MD5! if you want Sha1 just change all .md5() to .sha1()
-    input=input('Filename to compare:')
+    input=input('File PATH to compare:')
     open=open(input)
     reading=open.read(128)
     encoding=reading.encode('utf8')
